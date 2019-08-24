db8cc6b29e1af3f768948ef5a0e31df811528685
kevintuchman <kevin.a.tuchman@gmail.com>
2019-08-24 13:47:15 -0700

    js and index

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var cookieListEl = d
+
+var pike  = {
+  min: '23',
+  max: '65',
+  sale: '6.3'
+}
+
+var seatac  =  {
+  min: '3',
+  max: '24',
+  sale: '1.2'
+}
+
+var seacenter = {
+  min: '11',
+  max: '38',
+  sale: '3.7'
+}
+
+var capitol = {
+  min: '20',
+  max: '38',
+  sale: '2.3'
+}
+
+var alki = {
+  min: '2',
+  max: '16',
+  sale: '4.6'
+}
