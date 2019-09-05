b12b0ab2a5fcee4a27a88334b1ca3cd30c4332e3
kevintuchman <kevin.a.tuchman@gmail.com>
2019-09-04 19:15:45 -0700

    new branch

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cookie-stand
+
+https://raw.githubusercontent.com/codefellows/seattle-201n15/master/class-06/lab/assets/salmon.png
--- js/app.js
+++ js/app.js
@@ -1,33 +1,93 @@
 'use strict';
 
-var cookieListEl = d
+//  an array to hold the hours
+//  variable to hold the ul in the DOM
 
+//  object literal for each location
+//  name of store
+//  min
+//  max
+//  avg cookies per cust
+//  a cookies sold each hour array
+//  a customers per hour array
+//  total for the location  
+
+//  method: calculates the cookies sold each hour
+
+//  helper function to calculate a random number
+
+// 1st and Pike
 var pike  = {
-  min: '23',
-  max: '65',
-  sale: '6.3'
-}
+  name: 'first and Pike',
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookiesPerCustomers :6.3,
 
+  cookiesSoldEachHourArr: [],
+  customersPerHourArry: [],
+  totalPerDay: 0,
+
+  calcCustomersEachHour:  function(){
+    for(var i = 0; i < hours.length; i++)
+      var customersEachHour = generateRandom
+      (this.minCustomers, this.maxCustomers);
+
+    this.customersPerHourArr.push(customersEachHour);
+  },
+
+  calcCookiesSoldEachHour:  function(){
+    for(var i = 0; i< this.customersPerHourArry; i++)
+      var oneHourOfSales = this.customersPerHourArr[i] * this.avgCookiesPerCustomer;
+
+    this.cookiesSoldEachHourArr.push(oneHourofSales);
+  };
+  render: function(){
+    // MUST generate customers each hour BEFORE generating cookies each hour
+    this.calcCookiesSoldEachHour();
+    this.calcCustomersEachHour();
+
+};
+
+    // 1. create an element
+    var liEl = document.createElement('li');
+    // 2. give it content
+    liEl.textContent = this.name;
+    // 3. append it to the DOM
+    salesEl.appendChild(liEL);
+
+    for(var i = 0; i < hours.length; i++){
+      var liEl = document.createElement('li');
+      liEl.textContent = ``${hours[i]}: ${this.cookies}
+    }
+  }
+
+function generateRandom(mix, max){
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+// SeaTac Airport
 var seatac  =  {
   min: '3',
   max: '24',
-  sale: '1.2'
-}
+  sale: '1.2',
+};
 
+// Seattle Center
 var seacenter = {
-  min: '11',
-  max: '38',
-  sale: '3.7'
-}
+  min: 11,
+  max: 38,
+  sale: 3.7,
+};
 
+//Capitol Hill
 var capitol = {
-  min: '20',
-  max: '38',
-  sale: '2.3'
-}
+  min: 20,
+  max: 38,
+  sale: 2.3,
+};
 
+// Alki
 var alki = {
-  min: '2',
-  max: '16',
-  sale: '4.6'
-}
+  min: 2,
+  max: 16,
+  sale: 4.6,
+};
