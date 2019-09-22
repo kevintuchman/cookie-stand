23d797c8ebfbd8cb6f9dee6383d2297a07b15822
kevintuchman <kevin.a.tuchman@gmail.com>
2019-09-21 21:13:39 -0700

    worked with chris to make the js function now

--- js/app.js
+++ js/app.js
@@ -2,14 +2,37 @@
 
 var allStoreArr = [];
 
-function Store(name, min, max, sale){
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+function Store(name, min, max, avg){
   this.name = name;
   this.min = min;
   this.max = max ;
-  this.sale = sale;
+  this.avg = avg;
+  this.cookiesSold = [];
+  
 
   allStoreArr.push(this);
 }
+
+Store.prototype.calcEachHour = function() {
+  //method
+  var numberofCustomers = random(this.min, this.max);
+  var sold = numberofCustomers * this.avg;
+  return sold; 
+}
+
+Store.prototype.populateData = function() {
+  for(var i=0; i < hours.length; i++){
+    var cookiesSold = this.calcEachHour();
+    this.cookiesSold.push(cookiesSold);
+  }
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 
 new Store('1st and Pike', 23, 65, 6.3);
 new Store('SeaTac Airport', 3, 24, 1.2);
